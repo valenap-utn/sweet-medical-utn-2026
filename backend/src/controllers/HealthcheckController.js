5b42baf23a3166a03c0d08ea2753ac8a32452f66
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-04-16 11:05:23 -0300

    feat: healthcheck - Mejoras para el manejo de routes

---
+++ backend/src/controllers/HealthcheckController.js
@@ -0,0 +1,8 @@
+export const healthcheck = async (req, res) => {
+    try{
+        res.status(200).json({status: "OK", success: true});
+    }catch(err){
+        console.log(err);
+        res.json({status: "ERROR",success: false});
+    }
+}
