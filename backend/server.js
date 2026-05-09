24094ab193075e77d46ffa35a2ac9dc2f031ab1f
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-05-08 21:49:46 -0300

    feat(errors): agrego manejo de errores - errorHandler - errorLogger - notFoundHandler - AppError - y agrego archivos sin funcionalidad aún (PlanController, PlanRepository y PlanService)

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import app from "./app.js";
 import 'dotenv/config';
+// import {connectDB} from "./src/config/db.js";
 
 const port = process.env.PORT || 4000;
+const host = process.env.HOST;
+
+// await connectDB();
 
 app.listen(port, () => {
-    console.log(`🚀 Servidor corriendo en http://localhost:${port}`)
+    console.log(`🚀 Servidor corriendo en http://${host}:${port}`)
 })
