f82d0d0d43738ea9ba7b033bb83804bbda868f74
Cynthia Abbate <82001339+Cynthia-21@users.noreply.github.com>
2026-05-27 12:59:48 -0300

    Add method to get day name in Spanish

--- backend/src/domain/Agenda.js
+++ backend/src/domain/Agenda.js
@@ -155,4 +155,18 @@ export class Agenda {
 
         return slots;
     }
+
+    _nombreDiaSemanaES(getDay) {
+        const mapa = {
+             0: 'Domingo',
+             1: 'Lunes',
+             2: 'Martes',
+             3: 'Miercoles',
+             4: 'Jueves',
+             5: 'Viernes',
+             6: 'Sabado',
+         };
+         return mapa[getDay] ?? '';
+    }
 }
+
