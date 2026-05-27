7a9c1b8706271eb4032e1894d92e7892aa8ef639
Cynthia Abbate <82001339+Cynthia-21@users.noreply.github.com>
2026-05-27 13:05:32 -0300

    Enable timestamps in PracticaSchema

--- backend/src/schemas/coberturas/practicaSchema.js
+++ backend/src/schemas/coberturas/practicaSchema.js
@@ -27,11 +27,11 @@ const PracticaSchema = new mongoose.Schema({
         required: true,
     }
 }, {
-    _id: false,
-    timestamps: false
+    timestamps: true
 })
 
 PracticaSchema.loadClass(Practica);
 
+export const PracticaModel = mongoose.model("Practica", PracticaSchema);
 export { PracticaSchema };
 
