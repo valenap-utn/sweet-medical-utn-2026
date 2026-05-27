4499d921a7e77e04ffb3ddd21fe374bfb367c30f
Cynthia Abbate <82001339+Cynthia-21@users.noreply.github.com>
2026-05-27 12:40:02 -0300

    Refactor Plan class to improve coverage methods

--- backend/src/domain/coberturas/Plan.js
+++ backend/src/domain/coberturas/Plan.js
@@ -1,25 +1,21 @@
 import {PlanInvalido} from "../../exceptions/PlanInvalido.js";
 import {NivelCobertura} from "../enums/NivelCobertura.js";
 import {CoberturaInvalida} from "../../exceptions/CoberturaInvalida.js";
-
 export class Plan {
     id;
     nombre;
     coberturasEspecialidad = [];
     coberturasPractica = [];
-
     constructor(nombre) {
         this.validarParametros(nombre);
         this.nombre = nombre;
     }
-
     validarParametros(nombre) {
         if ([nombre].some(v => !v)) {
             throw new PlanInvalido(`El plan necesita un nombre.\n
                 Se recibió nombre: ${nombre}`);
         }
     }
-
     obtenerCoberturaEspecialidad(especialidad) {
         if(!especialidad.id){
             throw new CoberturaInvalida(`La cobertura para ${especialidad} no pudo ser encontrada`);
@@ -27,7 +23,6 @@ export class Plan {
         const tipoCobertura = this.coberturasEspecialidad.find(c => c.especialidad.id === especialidad.id);
         return tipoCobertura ? tipoCobertura.nivel : NivelCobertura.NO_CUBIERTA;
     }
-
     obtenerCoberturaPractica(practica) {
         if(!practica.id){
             throw new CoberturaInvalida(`La cobertura para ${practica} no pudo ser encontrada`);
@@ -35,7 +30,6 @@ export class Plan {
         const tipoCobertura = this.coberturasPractica.find(c => c.practica.id === practica.id);
         return tipoCobertura ? tipoCobertura.nivel : NivelCobertura.NO_CUBIERTA;
     }
-
     agregarEspecialidad(coberturaEspecialidad) {
         if(!this.coberturasEspecialidad.includes(coberturaEspecialidad)) {
             this.coberturasEspecialidad.push(coberturaEspecialidad);
@@ -43,7 +37,6 @@ export class Plan {
             console.log(`La especialidad ${coberturaEspecialidad} ya pertenece al plan ${this.nombre}`);
         }
     }
-
     agregarPractica(coberturaPractica){
         if(!this.coberturasPractica.includes(coberturaPractica)){
             this.coberturasPractica.push(coberturaPractica);
