475d53306ff30d2d5507c0cab6526316c3c12a9e
enzoobona <ebonafine@frba.utn.edu.ar>
2026-06-03 23:53:10 -0300

    Tests terminados para mergear a entrega-2

--- backend/src/services/ServiciosMedicoService.js
+++ backend/src/services/ServiciosMedicoService.js
@@ -5,21 +5,21 @@ export class ServiciosMedicoService {
     }
 
     async crearEspecialidad({nombre, duracionTurnoEnMins, costoConsulta}){
-        if(await this.especialidadRepository.findOne(nombre, duracionTurnoEnMins, costoConsulta)) throw new Error("La especialidad ya existe");
+        if(await this.especialidadRepository.findOne(nombre, duracionTurnoEnMins, costoConsulta)) throw new Error(`La especialidad ya existe`);
 
         return await this.especialidadRepository.create({nombre, duracionTurnoEnMins, costoConsulta});
     }
 
     async borrarEspecialidad({especialidadId}){
         const especialidad = await this.especialidadRepository.findById(especialidadId);
-        if(!especialidad) throw new Error("La especialidad no existe");
+        if(!especialidad) throw new Error(`La especialidad ${especialidadId} no existe`);
 
         return await this.especialidadRepository.findByIdAndDelete(especialidadId);
     }
 
     async modificarEspecialidad(especialidadId, {nombre, duracionTurnoEnMins, costoConsulta}){
         const especialidad = await this.especialidadRepository.findById(especialidadId);
-        if(!especialidad) throw new Error("La especialidad no existe");
+        if(!especialidad) throw new Error(`La especialidad ${especialidadId} no existe`);
 
         especialidad.establecerNuevoNombre(nombre);
         especialidad.establecerNuevaDuracion(duracionTurnoEnMins);
@@ -29,21 +29,21 @@ export class ServiciosMedicoService {
     }
 
     async crearPractica({nombre, duracionTurnoEnMins, costo}){
-        if(await this.practicaRepository.findOne(nombre, duracionTurnoEnMins, costo)) throw new Error("La practica ya existe");
+        if(await this.practicaRepository.findOne(nombre, duracionTurnoEnMins, costo)) throw new Error(`La practica ya existe`);
 
         return await this.practicaRepository.create({nombre, duracionTurnoEnMins, costo});
     }
 
     async borrarPractica({practicaId}){
         const practica = await this.practicaRepository.findById(practicaId);
-        if(!practica) throw new Error("La practica no existe");
+        if(!practica) throw new Error(`La practica ${practicaId} no existe`);
 
         return await this.practicaRepository.findByIdAndDelete(practicaId);
     }
 
     async modificarPractica(practicaId, {nombre, duracionTurnoEnMins, costo}){
         const practica = await this.practicaRepository.findById(practicaId);
-        if(!practica) throw new Error("La practica no existe");
+        if(!practica) throw new Error(`La practica ${practicaId} no existe`);
 
         practica.establecerNuevoNombre(nombre);
         practica.establecerNuevaDuracion(duracionTurnoEnMins);
