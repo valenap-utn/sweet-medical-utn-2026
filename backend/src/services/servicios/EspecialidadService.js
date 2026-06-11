d71c871cba654a4e87586990d94521d8ee5c4ae8
Valentina Andrada Perino <vandradaperino@frba.utn.edu.ar>
2026-06-11 14:32:21 -0300

    style(servicios): comentarios - deje comentados los métodos que estan en ServiciosMedicoService.js para:   - luego volar ese servicio y dejar los métodos (comentados) en los correspondientes servicios

--- backend/src/services/servicios/EspecialidadService.js
+++ backend/src/services/servicios/EspecialidadService.js
@@ -23,4 +23,33 @@ export class EspecialidadService {
         await this.especialidadRepository.delete(especialidadId);
         return {mensaje: "Especialidad eliminada correctamente."}
     }
+
+    // Métodos traídos de ServiciosMedicoService (después arreglar, para + validaciones, no lo hago ahora porque estoy con otra cosa)
+    /*async crearEspecialidad({nombre, duracionTurnoEnMins, costo}) {
+        if (!nombre || !duracionTurnoEnMins || costo == null) {
+            throw new BadRequestError(`Debe indicar nombre, duración del turno y costo de consulta para crear la especialidad.`)
+        }
+
+        if (await this.especialidadRepository.findOne(nombre, duracionTurnoEnMins, costo)) throw new ConflictError(`Ya existe una especialidad con nombre ${nombre}, duración ${duracionTurnoEnMins} minutos y costo ${costo}.`);
+
+        return await this.especialidadRepository.create({nombre, duracionTurnoEnMins, costo});
+    }
+
+    async borrarEspecialidad({especialidadId}) {
+        const especialidad = await this.especialidadRepository.findById(especialidadId);
+        if (!especialidad) throw new NotFoundError(`La especialidad con id: ${especialidadId} no fue encontrada.`);
+
+        return await this.especialidadRepository.findByIdAndDelete(especialidadId);
+    }
+
+    async modificarEspecialidad(especialidadId, {nombre, duracionTurnoEnMins, costo}) {
+        const especialidad = await this.especialidadRepository.findById(especialidadId);
+        if (!especialidad) throw new NotFoundError(`La especialidad con id: ${especialidadId} no fue encontrada.`);
+
+        especialidad.establecerNuevoNombre(nombre);
+        especialidad.establecerNuevaDuracion(duracionTurnoEnMins);
+        especialidad.establecerNuevoCosto(costo);
+
+        return await this.especialidadRepository.save(especialidad);
+    }*/
 }
--- backend/src/services/servicios/PracticaService.js
+++ backend/src/services/servicios/PracticaService.js
@@ -5,21 +5,50 @@ export class PracticaService {
         this.practicaRepository = practicaRepository;
     }
 
-    async crear(data){
+    async crear(data) {
         return await this.practicaRepository.create(data);
     }
 
-    async obtenerTodas(){
+    async obtenerTodas() {
         return await this.practicaRepository.findAll();
     }
 
-    async obtenerPorId(id){
+    async obtenerPorId(id) {
         return await this.practicaRepository.findById(id);
     }
 
-    async eliminar(practicaId){
+    async eliminar(practicaId) {
         const practica = await this.practicaRepository.findById(practicaId);
-        if(!practica) throw new NotFoundError(`No se encontró la práctica a eliminar.`);
+        if (!practica) throw new NotFoundError(`No se encontró la práctica a eliminar.`);
         return await this.practicaRepository.delete(practicaId);
     }
+
+    // Métodos traídos de ServiciosMedicoService (después arreglar, para + validaciones, no lo hago ahora porque estoy con otra cosa)
+    /*async crearPractica({nombre, duracionTurnoEnMins, costo}) {
+        if (!nombre || !duracionTurnoEnMins || costo == null) {
+            throw new BadRequestError("Debe indicar nombre, duración del turno y costo para crear la práctica.");
+        }
+
+        if (await this.practicaRepository.findOne(nombre, duracionTurnoEnMins, costo)) throw new ConflictError(`La practica ${nombre} ya existe.`);
+
+        return await this.practicaRepository.create({nombre, duracionTurnoEnMins, costo});
+    }
+
+    async borrarPractica({practicaId}) {
+        const practica = await this.practicaRepository.findById(practicaId);
+        if (!practica) throw new NotFoundError(`La practica con id: ${practicaId} no fue encontrada.`);
+
+        return await this.practicaRepository.findByIdAndDelete(practicaId);
+    }
+
+    async modificarPractica(practicaId, {nombre, duracionTurnoEnMins, costo}) {
+        const practica = await this.practicaRepository.findById(practicaId);
+        if (!practica) throw new NotFoundError(`La practica con id: ${practicaId} no fue encontrada.`);
+
+        practica.establecerNuevoNombre(nombre);
+        practica.establecerNuevaDuracion(duracionTurnoEnMins);
+        practica.establecerNuevoCosto(costo);
+
+        return await this.practicaRepository.save(practica);
+    }*/
 }
